341474c62b8745a9f8a2e9a05b57a063a06055fc
Chappidi Mahathi <mahathichappidi@gmail.com>
2026-07-07 18:52:23 +0530

    Added dark mode, history, and UI improvements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".btn");
-
+const historyList = document.getElementById("historyList");
 buttons.forEach(function(button) {
 
     button.addEventListener("click", function() {
@@ -15,7 +15,15 @@ buttons.forEach(function(button) {
         }
         else if (value === "=") {
             try {
-                display.value = eval(display.value);
+            const expression = display.value;
+            const result = eval(expression);
+
+            display.value = result;
+
+            const item = document.createElement("li");
+            item.textContent = `${expression} = ${result}`;
+
+           historyList.prepend(item);
             } catch {
                 display.value = "Error";
             }
@@ -25,5 +33,57 @@ buttons.forEach(function(button) {
         }
 
     });
+
+});
+document.addEventListener("keydown", function (event) {
+
+    const key = event.key;
+
+    // Numbers and operators
+    if ("0123456789+-*/.%".includes(key)) {
+        display.value += key;
+    }
+
+    // Enter key (=)
+    else if (key === "Enter") {
+        event.preventDefault();
+
+        try {
+            display.value = eval(display.value);
+        } catch {
+            display.value = "Error";
+        }
+    }
+
+    // Backspace (DEL)
+    else if (key === "Backspace") {
+        display.value = display.value.slice(0, -1);
+    }
+
+    // Escape (C)
+    else if (key === "Escape") {
+        display.value = "";
+    }
+
+});
+const themeBtn = document.getElementById("themeBtn");
+
+// Load saved theme
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light");
+    themeBtn.textContent = "☀️";
+}
+
+themeBtn.addEventListener("click", function(){
+
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        themeBtn.textContent = "☀️";
+        localStorage.setItem("theme","light");
+    }else{
+        themeBtn.textContent = "🌙";
+        localStorage.setItem("theme","dark");
+    }
 
 });
